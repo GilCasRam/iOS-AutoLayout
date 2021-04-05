f7dfe6fa227cce560ba00ea7407c1743f4a3e9f6
Gil CasRam <gcasram7@gmail.com>
2021-04-05 14:45:08 -0500

    Initial Commit

---
+++ AvancedAutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AvancedAutoLayout
+//
+//  Created by Gil CasRam on 05/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
